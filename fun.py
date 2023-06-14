b472903150e329de931f9e5174b5fd1f3577c611
catalinaa-paredes <catalinaparedes1604@gmail.com>
2023-06-13 22:35:39 -0400

    agregar comentarios

--- fun.py
+++ fun.py
@@ -1,11 +1,13 @@
+#definir si es primo o no
 def es_primo(numero):
-    if numero < 2:  
+    if numero < 2:   #numeros menores que dos no son primos
         return False
-    for i in range(2, int(numero**0.5) + 1):
+    for i in range(2, int(numero**0.5) + 1): #funcion 
         if numero % i == 0: 
-            return False
+            return False  #verificar y retornar
     return True
 
+#preguntar y responder
 numero = int(input("Ingresa un número: "))
 if es_primo(numero):
     print(f"{numero} es un número primo.")
