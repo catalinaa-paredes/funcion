ad9dab917e851fdea552635f2ce8c9ceae8835b7
catalinaa-paredes <catalinaparedes1604@gmail.com>
2023-06-13 22:27:02 -0400

    ingresar un numero

--- fun.py
+++ fun.py
@@ -5,3 +5,9 @@ def es_primo(numero):
         if numero % i == 0: 
             return False
     return True
+
+numero = int(input("Ingresa un número: "))
+if es_primo(numero):
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
