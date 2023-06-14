638c02d9117b291f9432732ac0504f80fa24bf83
catalinaa-paredes <catalinaparedes1604@gmail.com>
2023-06-13 22:42:01 -0400

    crear segunda funcion

--- fun.py
+++ fun.py
@@ -13,3 +13,22 @@ if es_primo(numero):
     print(f"{numero} es un número primo.")
 else:
     print(f"{numero} no es un número primo.")
+
+
+
+
+
+def obtener_numeros_primos_menores(numero):
+    numeros_primos = []
+    for num in range(2, numero):
+        if es_primo(num):
+            numeros_primos.append(num)
+    return numeros_primos
+
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
